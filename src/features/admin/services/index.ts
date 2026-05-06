081f3b5aa626bd898bab0d4afd7f0d6dd2570e69
Alexandre Janaina <alexandre.janaina@example.com>
2026-05-06 15:39:42 -0300

    chore: exporta serviços administrativos

---
+++ src/features/admin/services/index.ts
@@ -0,0 +1,6 @@
+export * from './adminAccess.service';
+export * from './adminAuditLogs.service';
+export * from './adminChannelsCache.service';
+export * from './adminClients.service';
+export * from './adminDevices.service';
+export * from './adminIptvSources.service';
